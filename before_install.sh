82d79ccb0c0d80779f0f1c6e736bda889b79f539
Ben Combee <combee@techwood.org>
2015-07-16 10:53:28 -0500

    Update before_install.sh
    One more fix for handling cached build

--- before_install.sh
+++ before_install.sh
@@ -8,20 +8,20 @@ cd ~
 # Get the Pebble SDK and toolchain
 PEBBLE_SDK_VER=${PEBBLE_SDK#PebbleSDK-}
 if [ ! -d $HOME/pebble-dev/${PEBBLE_SDK} ]; then
-  wget https://sdk.getpebble.com/download/${PEBBLE_SDK_VER} -O PebbleSDK.tar.gz
+  wget https://sdk.getpebble.com/download/${PEBBLE_SDK_VER} -O PebbleSDK-${PEBBLE_SDK_VER}.tar.gz
   wget http://assets.getpebble.com.s3-website-us-east-1.amazonaws.com/sdk/arm-cs-tools-ubuntu-universal.tar.gz
   # Build the Pebble directory
   mkdir -p ~/pebble-dev
-  cd ~/pebble-dev
   # Extract the SDK
-  tar -zxf ~/PebbleSDK.tar.gz
+  tar zxf PebbleSDK-${PEBBLE_SDK_VER}.tar.gz -C ~/pebble-dev/
   # Extract the toolchain
+  tar zxf arm-cs-tools-ubuntu-universal.tar.gz -C ~/pebble-dev/${PEBBLE_SDK}
+
+  # Install the Python library dependencies locally
   cd ~/pebble-dev/${PEBBLE_SDK}
-  tar -zxf ~/arm-cs-tools-ubuntu-universal.tar.gz
+  virtualenv --no-site-packages .env
+  source .env/bin/activate
+  pip install -r requirements.txt
+  deactivate
 fi
 
-# Install the Python library dependencies locally
-virtualenv --no-site-packages .env
-source .env/bin/activate
-pip install -r requirements.txt
-deactivate
