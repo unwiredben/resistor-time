0c0fdde72f31f4769e40c979848b1d088cd83d81
Ben Combee <combee@techwood.org>
2015-07-16 10:23:56 -0500

    Update pBuild for travis containers
    Remove sudo use
    Switch to whitelisted apt packages
    Cache tool directories

--- before_install.sh
+++ before_install.sh
@@ -1,4 +1,5 @@
-#!/usr/bin/env bash
+#!/bin/bash
+set -e
 echo 'pBuild 1.0'
 echo 'Installing Pebble SDK and its Dependencies...'
 
@@ -6,27 +7,18 @@ cd ~
 
 # Get the Pebble SDK and toolchain
 PEBBLE_SDK_VER=${PEBBLE_SDK#PebbleSDK-}
-wget https://sdk.getpebble.com/download/${PEBBLE_SDK_VER} -O PebbleSDK.tar.gz
-wget http://assets.getpebble.com.s3-website-us-east-1.amazonaws.com/sdk/arm-cs-tools-ubuntu-universal.tar.gz
-
-# Build the Pebble directory
-mkdir ~/pebble-dev
-
-cd ~/pebble-dev
-
-# Extract the SDK
-tar -zxf ~/PebbleSDK.tar.gz
-
-cd ~/pebble-dev/${PEBBLE_SDK}
-
-# Extract the toolchain
-tar -zxf ~/arm-cs-tools-ubuntu-universal.tar.gz
-
-# (Add the pebble tool to your path and reload your shell configuration) isn't used because it fails
-
-# Install pip and virtualenv
-sudo apt-get install python-pip python2.7-dev
-sudo pip install virtualenv
+if [! -d $HOME/pebble-dev/${PEBBLE_SDK}]; then
+  wget https://sdk.getpebble.com/download/${PEBBLE_SDK_VER} -O PebbleSDK.tar.gz
+  wget http://assets.getpebble.com.s3-website-us-east-1.amazonaws.com/sdk/arm-cs-tools-ubuntu-universal.tar.gz
+  # Build the Pebble directory
+  mkdir ~/pebble-dev
+  cd ~/pebble-dev
+  # Extract the SDK
+  tar -zxf ~/PebbleSDK.tar.gz
+  # Extract the toolchain
+  cd ~/pebble-dev/${PEBBLE_SDK}
+  tar -zxf ~/arm-cs-tools-ubuntu-universal.tar.gz
+fi
 
 # Install the Python library dependencies locally
 virtualenv --no-site-packages .env
