de474f3912100bcbd86e09bfdab9daba39a40215
Ben Combee <combee@techwood.org>
2015-07-16 10:38:47 -0500

    Fix mkdir to use -p

--- before_install.sh
+++ before_install.sh
@@ -11,7 +11,7 @@ if [ ! -d $HOME/pebble-dev/${PEBBLE_SDK} ]; then
   wget https://sdk.getpebble.com/download/${PEBBLE_SDK_VER} -O PebbleSDK.tar.gz
   wget http://assets.getpebble.com.s3-website-us-east-1.amazonaws.com/sdk/arm-cs-tools-ubuntu-universal.tar.gz
   # Build the Pebble directory
-  mkdir ~/pebble-dev
+  mkdir -p ~/pebble-dev
   cd ~/pebble-dev
   # Extract the SDK
   tar -zxf ~/PebbleSDK.tar.gz
